9aae9ea79cbbd0bfd49b4c117a01ce01c0109c58
CATRolos <carlosoarde@gmail.com>
2021-11-16 16:47:00 +0800

    Changed enemy to have distance based ai

--- Assets/monokumaController.cs
+++ Assets/monokumaController.cs
@@ -8,19 +8,29 @@ public class monokumaController : MonoBehaviour
     Rigidbody2D rb;
     Vector2 movement;
     public float speed;
-    public float rotationSpeed;
+    public float stopDistance;
+    public float retreatDistance;
+
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        movement = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f));
-        MoveKuma(movement);
+        player = GameObject.Find("galaOmega").transform;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-    
+        if(Vector2.Distance(transform.position, player.position) > stopDistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stopDistance && Vector2.Distance(transform.position, player.position) > retreatDistance){
+
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
     }
     void MoveKuma(Vector2 vel){
         rb.velocity = vel * speed;
